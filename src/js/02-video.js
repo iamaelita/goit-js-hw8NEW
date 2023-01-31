3fbe7cbdb7f7ac922a948ea8cc50c90597e0ca3d
Aelita Deinychenko <amm-aelita@ukr.net>
2023-01-31 11:05:16 +0200

    task2 upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const paused = 'videoplayer-current-time';
+
+
+const onPlay = function (data) {
+  localStorage.setItem(paused, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedTime = localStorage.getItem(paused);
+if(savedTime){
+  player.setCurrentTime(savedTime);
+}
